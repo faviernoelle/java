ad203d43aace9b2fea2c611ccdd73434f2a76063
Noelle FAVIER <favier.noelle@gmail.com>
2016-10-27 13:15:14 +0200

    intialize git rep

---
+++ ImprovedHelloWorld/src/ense3/MyHelloWorld.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ense3;
+
+/**
+ *
+ * @author Noëlle
+ */
+public class MyHelloWorld {
+    
+    public static void main(String[] args) {
+for(int i=0; i<5; i++)
+System.out.println("Hello World "+i+"!");
+    }
+}
